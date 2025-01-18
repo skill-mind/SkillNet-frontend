577d9eeb42a2955d66e146585079e8fe0ec1810f
tali-creator <talinanzing111@gmail.com>
2025-01-18 02:30:33 +0100

    Help Desk

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Work_Sans, Raleway } from "next/font/google";
 
 export const WorkSans = Work_Sans({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
+  weight: ["300", "400", "500", "700"],
 });
 export const RaLeway = Raleway({
   subsets: ["latin"],
