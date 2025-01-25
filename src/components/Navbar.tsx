917beea461451bc714e9dc7ed34dd631e1081851
IO_Savy <onodairo449449@gmail.com>
2025-01-25 23:48:12 +0100

    # IMPLEMENT THE CONNECT BUTTON MODAL (#33)
    * # IMPLEMENT THE CONNECT BUTTON MODAL
    Fixes #19
    
    * fix: excessive space fixed
    
    * feat: fixing bug
    
    * # IMPLEMENT THE CONNECT BUTTON MODAL
    Fixes #19
    
    * # IMPLEMENT THE CONNECT BUTTON MODAL
    Fixes #19
    
    * fix: remove unnecessary lorem text
    
    * fix
    
    * fix
    
    * # IMPLEMENT THE CONNECT BUTTON MODAL
    Fixes #19
    
    * fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
 "use client";
 import Logo from "../public/img/logo.svg";
 import Image from "next/image";
+import { useState } from "react";
+import { WalletSelectorUI } from "./WalletConnectModal";
+
 import Link from "next/link";
 
+
 export default function Navbar() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
   return (
     <nav className="w-full flex justify-between items-center px-[100px] py-[22px] bg-[#101110] text-sm leading-6 text-[#FCFCFC] fixed top-0 left-0 z-[5]">
       <button className="flex items-center cursor-pointer">
@@ -27,9 +37,17 @@ export default function Navbar() {
           <Link href="/faqs"> FAQs</Link>
         </li>
       </ul>
-      <button className="border border-[#313130] rounded-lg py-4 px-[35px] font-bold">
-        CONNECT WALLET
-      </button>
+      <div className="flex items-center flex-col relative">
+        <button onClick={showModal}  className="border border-[#313130] rounded-lg py-4 px-[35px] font-bold">
+          CONNECT WALLET
+        </button>
+        {isModalVisible && (
+          <div className="absolute top-full right-0">
+            <WalletSelectorUI />
+          </div>
+        )}
+      </div>
+
     </nav>
   );
 }
