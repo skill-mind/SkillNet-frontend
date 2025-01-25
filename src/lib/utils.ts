1833aa983a82300e25c993957a31c2e6e4d49bf8
Uyoxy <blessingtimothy20171@gmail.com>
2025-01-25 13:36:19 +0100

    Fix line endings to LF

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-// Helper function to shorten address
-export const shortenAddress = (address: string) => {
-    return `${address.slice(0, 6)}...${address.slice(-4)}`;
-  };
-  
-  // Helper function to convert hex to decimal and format it
-  export const formatAmount = (hex: string) => {
-    const decimal = parseInt(hex, 16);
-    return decimal.toString();
-  };
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
