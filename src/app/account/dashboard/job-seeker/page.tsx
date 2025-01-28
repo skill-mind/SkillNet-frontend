27b5820cdef3137a1bf9e0068ca749d5179fe3e1
Usman Alfaki <82191216+ussyalfaks@users.noreply.github.com>
2025-01-28 15:18:23 +0100

    feat: add navbar for job seeker dashboard (#73)
    * feat: add navbar for job seeker dashboard
    
    * feat: add navbar for job seeker dashboard

--- src/app/account/dashboard/job-seeker/page.tsx
+++ src/app/account/dashboard/job-seeker/page.tsx
@@ -1,8 +1,10 @@
 import JobSeeker from "./job-seeker";
+import NavbarJobSeeker from "@/components/Navbar-job-seeker";
 
 function page() {
   return (
     <div>
+        <NavbarJobSeeker />
         <JobSeeker />
     </div>
   );
