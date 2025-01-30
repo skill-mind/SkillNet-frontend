f52b4114fea4b60c2731fb960a2704ca460ffad2
Jemiiah <160767568+Jemiiah@users.noreply.github.com>
2025-01-30 19:48:55 +0000

    feat: my courses page

--- src/app/account/dashboard/learning-hub/components/MyCourses.tsx
+++ src/app/account/dashboard/learning-hub/components/MyCourses.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { Search } from "lucide-react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CourseCard from "./CourseCard";
 
 const MyCourses = () => {
-  return (
-    <div>MyCourses</div>
-  )
-}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-export default MyCourses
+  return (
+    <>
+      <div className="relative">
+        <div className="absolute top-[14px] left-3 h-fit mb-10">
+          <Search size={20} color="#ABABAB" />
+        </div>
+        <input
+          type="text"
+          className="py-3 pl-10 pr-6 rounded-[8px] text-sm leading-6 italic w-[587px] bg-[#1A1B1A] border border-[#1F1F1F] placeholder:text-[#696969]"
+          placeholder="Search For courses..."
+        />
+      </div>
+
+      <div className="h-[600px] pr-[39px] sm:h-[450px] sm:pr-[76px] mt-10">
+        <Slider {...settings}>
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default MyCourses;
--- src/app/account/dashboard/learning-hub/learningHub.tsx
+++ src/app/account/dashboard/learning-hub/learningHub.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import TabSwitcher from "./components/TabSwitcher";
+import MyCourses from "./components/MyCourses";
 
 const LearningHub = () => {
-  return (
-    <div>learningHub</div>
-  )
-}
+  const [currentTab, setCurrentTab] = useState<
+    "explore" | "my-courses" | "wish-list"
+  >("explore");
 
-export default LearningHub
+  function handleSwitch(to: "explore" | "my-courses" | "wish-list") {
+    if (to === currentTab) return;
+    setCurrentTab(to);
+  }
+  return (
+    <div className="py-4 px-[100px]">
+      <TabSwitcher activeTab={currentTab} handleSwitch={handleSwitch} />
+      <div className="py-[50px]">
+        {currentTab === "my-courses" && <MyCourses />}
+      </div>
+    </div>
+  );
+};
+
+export default LearningHub;
