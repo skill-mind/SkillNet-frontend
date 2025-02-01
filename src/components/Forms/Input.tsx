c6fd4c1a45a30e54ff8c2ede925a5c20173b38f4
robtz <danielpatrickblock@gmail.com>
2025-02-01 15:36:11 +0100

    updates

--- src/components/Forms/Input.tsx
+++ src/components/Forms/Input.tsx
@@ -1,4 +1,3 @@
-import { EllipsisVertical, Search } from "lucide-react";
 import React from "react";
 
 function Input({
@@ -18,21 +17,18 @@ function Input({
 }) {
   return (
     <div className="flex flex-col">
-      <label htmlFor={name} className="flex text-[#9596A0] text-[0.75rem] mb-1">
+      <label htmlFor={name} className="text-[#9596A0] text-[0.875rem] mb-1">
         {label}
       </label>
-      <div className="relative flex justify-between items-center border-[1px] border-[#252625] rounded-[0.5rem]">
-        <Search className="absolute left-4"  size={20} color="#cfd1cf"/>
-        <input
-          type={type}
-          name={name}
-          id={name}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          className="bg-transparent outline-none border-[#252625] text-white text-[0.75rem] w-full md:w-[20rem] pl-10 pr-4 py-[0.5rem] rounded-[0.5rem]" // Reduced size for input
-        />
-      </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="bg-transparent border-[1px] outline-none border-[#252625] text-white text-[0.875rem] w-full md:w-[26rem] px-4 py-[0.75rem] rounded-[0.5rem]"
+      />
     </div>
   );
 }
