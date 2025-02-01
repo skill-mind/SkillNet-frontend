071f49b00a4d170b0a871533889d73f6eebcc9a3
danielrobotz <85536328+danielrobotz@users.noreply.github.com>
2025-02-01 16:17:39 +0100

    feat implement Message page (#97)
    * draft pr
    
    * updates on message page
    
    * minor fix on message page
    
    * # IMPLEMENT THE MESSAGE PAGE FOR THE JOB SEEKER DASHBOARD #75
    
    * # IMPLEMENT THE MESSAGE PAGE FOR THE JOB SEEKER DASHBOARD #75
    
    * updates
    
    * updates
    
    * updates
    
    * updates

--- src/app/account/dashboard/messages/page.tsx
+++ src/app/account/dashboard/messages/page.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import Messages from "@/app/account/dashboard/messages/components/messages";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
- const page = () => {
-  return (
-    <div>page</div>
-  )
+function page(){
+    return(
+        <div>
+           <Navbar />
+                <Messages />
+           <Footer />
+        </div>
+    );
 }
-
 
 export default page;
