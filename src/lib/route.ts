d8b83d05eca95d54341668ab72cafb5dad39992f
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-02-02 16:23:15 +0100

    codebase destructure 1

---
+++ src/lib/route.ts
@@ -0,0 +1,10 @@
+export const routes = {
+    home:"/",
+    about:"/about",
+    helpDesk:"/help-desk",
+    faqs:"/faqs",
+    contact:"/contact-us",
+    codeOfEthic:"/code-of-ethics",
+    privacy:"/privacy",
+    terms:"/terms",
+}
