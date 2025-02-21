cddf863bf11ab24db5fc0616539070f958c6c600
sylvester-strk <85766371+sylvester-strk@users.noreply.github.com>
2025-02-21 15:20:26 +0100

    specified types

--- src/app/dashboard/tutor/component/CourseCard.tsx
+++ src/app/dashboard/tutor/component/CourseCard.tsx
@@ -1,28 +1,51 @@
-import { Edit2Icon } from "lucide-react";
+import {Edit2Icon} from "lucide-react";
 import Image from "next/image";
-function CourseCard({ course }: any) {
-    return (
-        <div className=" h-[218px] bg-[#161716] p-[24px] rounded-[8px] flex gap-[15px] ">
-            <section className="w-[300px] h-[170px]">
-                <Image src={course.banner} alt="course-banner" className="w-full h-full" />
-            </section>
-            <section className="flex-grow flex flex-col gap-[15px]">
-                <div className="flex justify-between">
-                    <div className="flex flex-col gap-3">
-                        <h3 className="font-[600] text-[#FCFCFC] text-[18px]">{course.title}</h3>
-                        <h4 className="font-[500] text-[#8DCAD7CC] text-[14px]">{course.videosAvailable ? "Video/Note guide available" : ""}</h4>
-                    </div>
-                    <div className="w-[82px] h-[32px] rounded-[48px] px-[14px] py-[4px] bg-[#2D2E2D] text-[12px] font-[500] flex justify-center items-center">{course.published ? "published" : "draft"}</div>
-                </div>
-                <div className="flex justify-between ">
-                    <div className="text-[#6E6E6E] font-[500] text-[14px]">Enrolment: <span className="text-white"> {course.enrollment} Learners</span></div>
-                    <div className="text-[#6E6E6E] font-[500] text-[14px]">Rating: <span className="text-white">{course.rating <= 0 ? "N/A" : `${course.rating}/5`}</span></div>
-                </div>
-                <button className="w-[110px] h-[30px] border-[#2F302F] border-[1px] border-solid rounded-[8px] font-[400] text-[12px] flex justify-center items-center gap-2"> <Edit2Icon className="w-[14px] h-[14px]" />Edit Course</button>
 
-            </section>
-        </div>
-    );
+type courseProps = {
+   id: number;
+   banner: any;
+   title: string;
+   enrollment: number;
+   rating: number;
+   published: boolean;
+   videosAvailable: boolean;
+};
+interface courseCardprops {
+   courses: courseProps;
 }
+
+const CourseCard: React.FC<courseCardprops> = ({courses}) => {
+   return (
+      <div className=" h-[218px] bg-[#161716] p-[24px] rounded-[8px] flex gap-[15px] ">
+         <section className="w-[300px] h-[170px]">
+            <Image src={courses.banner} alt="course-banner" className="w-full h-full" />
+         </section>
+         <section className="flex-grow flex flex-col gap-[15px]">
+            <div className="flex justify-between">
+               <div className="flex flex-col gap-3">
+                  <h3 className="font-[600] text-[#FCFCFC] text-[18px]">{courses.title}</h3>
+                  <h4 className="font-[500] text-[#8DCAD7CC] text-[14px]">{courses.videosAvailable ? "Video/Note guide available" : ""}</h4>
+               </div>
+               <div className="w-[82px] h-[32px] rounded-[48px] px-[14px] py-[4px] bg-[#2D2E2D] text-[12px] font-[500] flex justify-center items-center">
+                  {courses.published ? "published" : "draft"}
+               </div>
+            </div>
+            <div className="flex justify-between ">
+               <div className="text-[#6E6E6E] font-[500] text-[14px]">
+                  Enrolment: <span className="text-white"> {courses.enrollment} Learners</span>
+               </div>
+               <div className="text-[#6E6E6E] font-[500] text-[14px]">
+                  Rating: <span className="text-white">{courses.rating <= 0 ? "N/A" : `${courses.rating}/5`}</span>
+               </div>
+            </div>
+            <button className="w-[110px] h-[30px] border-[#2F302F] border-[1px] border-solid rounded-[8px] font-[400] text-[12px] flex justify-center items-center gap-2">
+               {" "}
+               <Edit2Icon className="w-[14px] h-[14px]" />
+               Edit Course
+            </button>
+         </section>
+      </div>
+   );
+};
 
 export default CourseCard;
--- src/app/dashboard/tutor/courses/courses.tsx
+++ src/app/dashboard/tutor/courses/courses.tsx
@@ -1,48 +1,57 @@
-import image1 from '../../../../public/img/course1.png'
-import image2 from '../../../../public/img/course2.png'
-import { Addcourse } from '../component/Addcourse';
-import CourseCard from '../component/CourseCard';
+import image1 from "../../../../public/img/course1.png";
+import image2 from "../../../../public/img/course2.png";
+import {Addcourse} from "../component/Addcourse";
+import CourseCard from "../component/CourseCard";
+
+type courseProps = {
+   id: number;
+   banner: any;
+   title: string;
+   enrollment: number;
+   rating: number;
+   published: boolean;
+   videosAvailable: boolean;
+};
+interface courseCardprops {
+   courses: courseProps;
+}
 function Courses() {
-  const courses = [
-    {
-      id: 1,
-      banner: image1,
-      title: "Become a web3 Developer:A beginners approach",
-      enrollment: 20,
-      rating: 4.5,
-      published: true,
-      videosAvailable: true
-
-    },
-    {
-      id: 2,
-      banner: image2,
-      title: "Become a web3 Developer:A beginners approach",
-      enrollment: 15,
-      rating: 0,
-      published: false,
-      videosAvailable: true
-    },
-  ]
-  return (
-    <div className=" px-8 h-auto pb-40 ">
-      <section className='flex flex-col gap-[15px]'>
-        {
-          courses.map((course) => {
-            return <CourseCard course={course} key={course.id} />
-          })
-        }
-      </section>
-      <div className='w-full flex justify-end'>
-        <button className=" my-4">View More</button>
+   const courses: courseProps[] = [
+      {
+         id: 1,
+         banner: image1,
+         title: "Become a web3 Developer:A beginners approach",
+         enrollment: 20,
+         rating: 4.5,
+         published: true,
+         videosAvailable: true,
+      },
+      {
+         id: 2,
+         banner: image2,
+         title: "Become a web3 Developer:A beginners approach",
+         enrollment: 15,
+         rating: 0,
+         published: false,
+         videosAvailable: true,
+      },
+   ];
+   return (
+      <div className=" px-8 h-auto pb-40 ">
+         <section className="flex flex-col gap-[15px]">
+            {courses.map((course) => {
+               return <CourseCard courses={course} key={course.id} />;
+            })}
+         </section>
+         <div className="w-full flex justify-end">
+            <button className=" my-4">View More</button>
+         </div>
+         <div className="w-full flex justify-start">
+            <button className=" my-4 capitalize">create a new course</button>
+         </div>
+         <Addcourse />
       </div>
-      <div className='w-full flex justify-start'>
-        <button className=" my-4 capitalize">create a new course</button>
-      </div>
-      <Addcourse />
-
-    </div>
-  );
+   );
 }
 
 export default Courses;
