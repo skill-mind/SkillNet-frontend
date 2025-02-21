23d5af139e84d42416598ada13124e05407c3480
Nze Shalom <106248195+Shalomnze@users.noreply.github.com>
2025-02-21 21:16:35 +0100

    #IMPLEMENT PAGE ROUTING ON THE INSTITUTION DASHBOARD (#131)
    * #IMPLEMENT PAGE ROUTING ON THE INSTITUTION DASHBOARD #IMPLEMENT THE VERIFICATION PAGE ON THE INSTITUTION DASHBOARD #124
    
    * Update package-lock.json

--- src/app/dashboard/institution/page.tsx
+++ src/app/dashboard/institution/page.tsx
@@ -1,54 +1,28 @@
-import DashboardCard from "@/components/DashboardCard";
-import PercentIcon from "@/svg/PercentIcon";
-import BriefcaseIcon from "@/svg/BriefcaseIcon";
-import MessageIcon from "@/svg/MessageIcon";
+"use client";
+import { useContext } from "react";
+import { DashBoardContext } from "@/app/useContext/dashboardContext";
+import Institution from "./institution/institution";
+import Candidates from "./candidates/page";
+import Notification from "./notification/page";
+import Certification from "./certificates/page";
+import Exam from "./exams/page";
+import Verification from "./verification/page";
 
-function page() {
+
+
+function Page() {
+  const { activeSection } = useContext(DashBoardContext);
+
   return (
-    <div className=" p-5 pr-[100px]">
-      <div className="grid grid-cols-3 gap-4 mb-[37px]">
-        <DashboardCard
-          background="#3498DB33"
-          color="#3498DB"
-          value="13"
-          item="Total Exams Hosted"
-          icon=<PercentIcon />
-        />
-        <DashboardCard
-          background="#BB6BD933"
-          color="#BB6BD9"
-          value="150"
-          item="Total Candidates"
-          icon=<BriefcaseIcon />
-        />
-        <DashboardCard
-          background="#BB6BD933"
-          color="#BB6BD9"
-          value="111"
-          item="Certificates Issued"
-          icon=<BriefcaseIcon />
-        />
-        <DashboardCard
-          background="#F2994A33"
-          color="#F39C12"
-          value="17"
-          item="Verification Requests"
-          icon=<MessageIcon />
-        />
-      </div>
-      <div>
-        <h4 className="text-sm leading-6 text-[#FCFCFC] mb-4">Quick Actions</h4>
-        <div className="flex items-center gap-x-5 text-sm leading-6 text-white font-bold">
-          <button className="py-3 w-[190px] text-center rounded-lg border border-[#2D2E2D]">
-            Create Exams
-          </button>
-          <button className="py-3 w-[190px] text-center rounded-lg border border-[#2D2E2D]">
-            View Candidates
-          </button>
-        </div>
-      </div>
+    <div className="overflow-y-auto scrollbar-hide scroll-smooth">
+      {activeSection === "home" && <Institution />}
+      {activeSection === "certifications" && <Certification />}
+      {activeSection === "notifications" && <Notification />}
+      {activeSection === "exam" && <Exam />}
+      {activeSection === "candidates" && <Candidates />}
+      {activeSection === "verification" && <Verification />}
+      
     </div>
   );
 }
-
-export default page;
+export default Page;
