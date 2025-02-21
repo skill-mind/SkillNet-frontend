324399842cfd67000ef04224ad3e08788fb36839
Oshioke-Salaki <salaki1902@gmail.com>
2025-02-21 13:01:40 +0100

    feat: institution dashboard

---
+++ src/svg/CandidatesIcon.tsx
@@ -0,0 +1,49 @@
+function CandidatesIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.08325 4.99967L6.66659 3.33301L11.2499 4.99967L9.16659 6.24967V7.49967C9.16659 7.49967 8.611 7.08301 6.66659 7.08301C4.72214 7.08301 4.16659 7.49967 4.16659 7.49967V6.24967L2.08325 4.99967ZM2.08325 4.99967V8.33301"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.16675 7.08301V7.82375C9.16675 9.25559 8.04746 10.4163 6.66675 10.4163C5.28604 10.4163 4.16675 9.25559 4.16675 7.82375V7.08301"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.7652 9.19117C12.7652 9.19117 13.1692 8.89708 14.5833 8.89708C15.9975 8.89708 16.4015 9.19117 16.4015 9.19117M12.7652 9.19117V8.33333L11.25 7.5L14.5833 6.25L17.9167 7.5L16.4015 8.33333V9.19117M12.7652 9.19117V9.43183C12.7652 10.436 13.5792 11.25 14.5833 11.25C15.5875 11.25 16.4015 10.436 16.4015 9.43183V9.19117"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3.65429 13.2717C2.86831 13.7104 0.807505 14.6064 2.06267 15.7274C2.67581 16.275 3.35869 16.6667 4.21723 16.6667H9.11625C9.97483 16.6667 10.6577 16.275 11.2708 15.7274C12.526 14.6064 10.4652 13.7104 9.67916 13.2717C7.83609 12.2428 5.49741 12.2428 3.65429 13.2717Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.3333 16.6663H16.4206C17.0645 16.6663 17.5767 16.353 18.0365 15.9149C18.9779 15.0181 17.4323 14.3013 16.8428 13.9503C15.7801 13.3175 14.4976 13.1713 13.3333 13.5114"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default CandidatesIcon;
---
+++ src/svg/HomeIcon.tsx
@@ -0,0 +1,32 @@
+function HomeIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 14.167H10.0075"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.6666 7.08301V11.2497C16.6666 14.3923 16.6666 15.9638 15.6903 16.94C14.714 17.9163 13.1426 17.9163 9.99992 17.9163C6.85722 17.9163 5.28588 17.9163 4.30956 16.94C3.33325 15.9638 3.33325 14.3923 3.33325 11.2497V7.08301"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+      <path
+        d="M18.3334 8.74967L14.7142 5.27924C12.4919 3.14842 11.3808 2.08301 10.0001 2.08301C8.61933 2.08301 7.50826 3.14842 5.28604 5.27924L1.66675 8.74967"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
+
+export default HomeIcon;
