fb91b5f2df85e6d8fced586ee513a49ad2c58ffa
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2025-02-21 18:43:19 +0100

    tutor-profile page (#129)
    * tutor-profile
    
    * tutor-profile
    
    * error
    
    * image error

--- src/app/dashboard/tutor/layout.tsx
+++ src/app/dashboard/tutor/layout.tsx
@@ -5,7 +5,7 @@ import DashBoardContextProvider from "@/app/useContext/dashboardContext";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <DashBoardContextProvider>
-      <main className="bg-[#101110] h-[100vh] max-h-screen overflow-y-hidden">
+      <main className="bg-[#101110] h-[100vh] max-h-screen  overflow-y-hidden">
         <Header />
         <div className="flex h-[100%] text-white overflow-y-auto scrollbar-hide scroll-smooth">
           <section className="overflow-y-hidden"> <Sidebar /></section>
