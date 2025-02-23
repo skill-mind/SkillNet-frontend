ccff27128054e80b6dea44d18d1e825a6c4bb84f
tali-creator <93847057+tali-creator@users.noreply.github.com>
2025-02-23 10:44:06 +0100

    create exam model

--- src/app/dashboard/institution/components/create-exam.tsx
+++ src/app/dashboard/institution/components/create-exam.tsx
@@ -9,6 +9,10 @@ interface Question {
   answer: string;
 }
 
+interface CreateExamModelProps {
+  onClose: () => void;
+}
+
 function CreateExamModel({ onClose }: CreateExamModelProps) {
   const [items, setItems] = useState<string[]>([]);
   const [files, setFiles] = useState<File[]>([]);
