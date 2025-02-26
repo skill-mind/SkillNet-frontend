9fb3e852ad3bbbebf727ef2640ac48564c908950
ekene leonard <nwobodoe71@gmail.com>
2025-02-26 19:35:48 +0100

    feat: add employer dashboard

--- src/app/dashboard/employer/layout.tsx
+++ src/app/dashboard/employer/layout.tsx
@@ -1,11 +1,44 @@
+"use client";
 
+import { useState } from "react";
+;
+import DashBoardContextProvider from "@/app/useContext/dashboardContext";
+import { Logs } from "lucide-react";
+import Header from "../tutor/component/DashboardHeader";
+import { Sidebar } from "./job-opening-notification/employer-sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <>
-        <main className="grow">{children}</main>
-      </>
-    );
-  };
-  
-  export default Layout;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  return (
+    <DashBoardContextProvider>
+      <main className="bg-[#101110] h-[90vh] max-h-screen">
+        <Header />
+        <div
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="md:hidden px-8"
+        >
+          <Logs size={30} className="text-white" />
+        </div>
+        <div className="flex h-[100%] text-white overflow-y-auto scrollbar-hide scroll-smooth">
+          {isSidebarOpen && (
+            <section className="">
+              {" "}
+              <Sidebar />
+            </section>
+          )}
+
+          <section className=" hidden md:block">
+            {" "}
+            <Sidebar />
+          </section>
+
+          <main className="flex-grow overflow-y-scroll scrollbar-hide h-auto hide-scrollbar">
+            {children}
+          </main>
+        </div>
+      </main>
+    </DashBoardContextProvider>
+  );
+};
+
+export default Layout;
--- src/app/dashboard/employer/page.tsx
+++ src/app/dashboard/employer/page.tsx
@@ -1,11 +1,32 @@
+"use client";
 
-import Employer from './employer';
-function page(){
-    return(
-        <div>
-           <Employer/>
-        </div>
-    );
+import Dashboard from "./employer";
+import { useContext } from "react";
+import { DashBoardContext } from "@/app/useContext/dashboardContext";
+
+
+function Page() {
+  const { activeSection } = useContext(DashBoardContext);
+
+  const renderContent = () => {
+    switch (activeSection) {
+      case "dashboard":
+        return <Dashboard />;
+      case "job openings":
+        return <div className="text-white">"job-opening"</div>;
+      case "notifications":
+        return <div className="text-white">"Notifications"</div>;
+
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  return (
+    <div className="mx-auto">
+      {renderContent()}
+    </div>
+  );
 }
 
-export default page;
+export default Page;
