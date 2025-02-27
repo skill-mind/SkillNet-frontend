c89c56f588a2cb567b0536b8ad81ce126aaa2095
tEmhItHoRpHe <tehmhithorphe@gmail.com>
2025-02-27 03:48:29 +0100

    Feature/implement the tutor earning weekly and monthly page #165 (#175)
    * chore: minor edit to dummy price to verify branch setup.
    
    * update: change bg color based on state.
    
    * add: date/time utility functions
    
    * update: earnings date labels now functional.
    
    * update: move dummy data to appropriate file.
    
    * chore: code refactor & restructure into component form.
    
    * chore: tiny eslint cleanup
    
    * chore: fix eslint errors

--- src/app/dashboard/tutor/component/DateActionButton.tsx
+++ src/app/dashboard/tutor/component/DateActionButton.tsx
@@ -1,13 +1,14 @@
 interface DateActionButtonProps {
   label: string;
+  isActive: boolean;
   onClick: () => void;
 }
 
-export function DateActionButton({ label, onClick }: DateActionButtonProps) {
+export function DateActionButton({ label, isActive, onClick }: DateActionButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="w-full px-3 py-3 bg-none border border-[#2D2E2D] hover:bg-[#FFFFFF1A] rounded-3xl transition-colors"
+      className={`${isActive ? "bg-[#FFFFFF1A]" : ""} w-full px-3 py-3 bg-none border border-[#2D2E2D] hover:bg-[#FFFFFF1A] rounded-3xl transition-colors`}
     >
       {label}
     </button>
--- src/app/dashboard/tutor/earnings/earnings.tsx
+++ src/app/dashboard/tutor/earnings/earnings.tsx
@@ -1,56 +1,54 @@
 "use client";
 
-import { Percent } from "lucide-react";
+import { useState } from "react";
 
 import { DateActionButton } from "../component/DateActionButton";
-import { EarningStatCard } from "../component/EarningStatCard";
-import { PaymentHistoryTable } from "../component/PaymentHistoryTable";
+import AllTime from "./components/AllTime";
+import Monthly from "./components/Monthly";
+import Weekly from "./components/Weekly";
+
+
+type DateLabel = "All-Time" | "Monthly" | "Weekly";
+
 
 function Earnings() {
+
+  // we set default date label view to "All-Time"
+  const [activeDateLabel, setActiveDateLabel] = useState<DateLabel>("All-Time");
+
+
   const DateActions = [
     {
       label: "All-Time",
-      onClick: () => console.log("All-Time clicked"),
-    },
-    {
-      label: "Weekly",
-      onClick: () => console.log("Weekly clicked"),
+      isActive: activeDateLabel === "All-Time",
+      onClick: () => setActiveDateLabel("All-Time"),
     },
     {
       label: "Monthly",
-      onClick: () => console.log("Montly clicked"),
+      isActive: activeDateLabel === "Monthly",
+      onClick: () => setActiveDateLabel("Monthly"),
+    },
+    {
+      label: "Weekly",
+      isActive: activeDateLabel === "Weekly",
+      onClick: () => setActiveDateLabel("Weekly"),
     },
   ];
 
-  const stats = [
-    {
-      value: "$1,500",
-      label: "Total Earnings",
-      icon: <Percent className="text-blue-500" size={30} />,
-      bgCol: "bg-blue-500",
-    },
-  ];
 
-  const transactions = [
-    {
-      id: 1,
-      transactionId: "0xe46d0b1039a8f97df2800...",
-      amount: "$15",
-      date: "12th Jan, 2025",
-    },
-    {
-      id: 2,
-      transactionId: "0xe46d0b1039a8f97df2800...",
-      amount: "$15",
-      date: "12th Jan, 2025",
-    },
-    {
-      id: 3,
-      transactionId: "0xe46d0b1039a8f97df2800...",
-      amount: "$15",
-      date: "12th Jan, 2025",
-    },
-  ];
+  const RenderComponent = () => {
+    switch (activeDateLabel) {
+      case "All-Time":
+        return <AllTime />;
+      case "Monthly":
+        return <Monthly />;
+      case "Weekly":
+        return <Weekly />;
+      default:
+        return null;
+    }
+  };
+
 
   return (
     <main className="flex-1 px-8 scrollbar-hide">
@@ -59,17 +57,8 @@ function Earnings() {
           <DateActionButton key={index} {...action} />
         ))}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-        {stats.map((stat, index) => (
-          <EarningStatCard key={index} {...stat} />
-        ))}
-      </div>
-      <section className="overflow-y-hidden mb-44">
-        <h3 className="text-lg font-semibold mb-8">Payment History</h3>
-        <div className="">
-          <PaymentHistoryTable transactions={transactions} />
-        </div>
-      </section>
+      {RenderComponent()}
+
     </main>
   );
 }
