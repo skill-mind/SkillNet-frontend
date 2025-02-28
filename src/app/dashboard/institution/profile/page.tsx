0fa4088ec40bbd5051cd417472bab94eca99e63e
danielrobotz <85536328+danielrobotz@users.noreply.github.com>
2025-02-28 20:05:19 +0000

    updates

--- src/app/dashboard/institution/profile/page.tsx
+++ src/app/dashboard/institution/profile/page.tsx
@@ -1,23 +1,11 @@
 "use client"
 
 import Image from "next/image";
-import { useState } from "react"; 
 import profileLogo from "../../../../public/institutionLogo.svg"
-import copy from "../../../../public/img/copy.png"
-import edit from "../../../../public/edit.svg"
 import ProfileEditModal from "./profile-edit-modal";
 import { Copy } from "lucide-react";
 
-interface Exam {
-  id: number;
-  status: string;
-}
-
 const Profile: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); 
-
-  const openModal = () => setIsModalOpen(true); 
-  const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className="bg-black min-h-screen p-6 md:mr-20 text-white flex flex-col relative">
@@ -56,9 +44,9 @@ const Profile: React.FC = () => {
         SkillNet Org is a forward-thinking design agency specializing in UI/UX, branding, and product design.
          We craft seamless, intuitive, and visually stunning experiences that captivate users and drive business growth. 
          Our team of creative strategists, designers, and developers collaborate to transform ideas into reality—whether
-          it's building digital products, designing brand identities, or optimizing user experiences. At Innovate Designs,
+          it&aposs building digital products, designing brand identities, or optimizing user experiences. At Innovate Designs,
            we believe in blending creativity with cutting-edge technology to deliver impactful solutions 
-           that stand out in today's competitive market. Let’s design the future, together.
+           that stand out in today&aposs competitive market. Let&aposs design the future, together.
         </p>
       </div>
     
