db4e7fb0c7b501ed943c046c2c1960a0cbcd0a8a
Bioba <97415163+Agbeleshe@users.noreply.github.com>
2025-02-28 22:36:02 +0000

    added chatbot icon base on figma

--- src/components/ChatBotButton.jsx
+++ src/components/ChatBotButton.jsx
@@ -2,6 +2,9 @@
 import React, { useState } from 'react';
 import { MessageSquare } from 'lucide-react';
 import ChatbotModal from './ChatbotModal';
+import robotic from '../public/img/robotic.svg'
+import Image from 'next/image';
+
 
 const ChatBotButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -14,8 +17,9 @@ const ChatBotButton = () => {
         onClick={openModal}
         className="flex items-center px-4 py-2 text-gray-300 hover:text-white hover:bg-[#FFFFFF1A] rounded-lg transition-colors w-full"
       >
-        <MessageSquare className="mr-3" size={20} />
-        <span>Chat Bot</span>
+
+        <Image className='mr-3' src={robotic} size={20} />
+        <span>AI Chatbot</span>
       </button>
       <ChatbotModal isOpen={isModalOpen} onClose={closeModal} />
     </>
