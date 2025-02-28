e6195c9a4613a01a7fb0f5da7ae4ecc43d384498
frankie-powers <franklin.power21@gmail.com>
2025-02-28 09:55:27 +0100

    fixed message component

--- src/app/dashboard/employer/notifications/components/messagescomponent.tsx
+++ src/app/dashboard/employer/notifications/components/messagescomponent.tsx
@@ -9,13 +9,11 @@ import {
   ChevronLeft,
 } from "lucide-react";
 import Image, { StaticImageData } from "next/image";
-import Input from "./input-messages";
 import { useState } from "react";
 import userProfile2 from "@/public/img/astronaut.svg";
 
 function Messages() {
-  const [selectedFilter, setSelectedFilter] = useState<string>("All"); // Track the selected filter
-  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedFilter, setSelectedFilter] = useState<string>("New"); // Track the selected filter
 
   // Sample messages (this can be replaced with actual dynamic data)
   const messages = [
@@ -25,7 +23,6 @@ function Messages() {
       message: "Hi, how are you?",
       status: "New",
     },
-
     {
       id: 2,
       name: "Elon Musk",
@@ -35,15 +32,8 @@ function Messages() {
   ];
 
   // Filter messages based on the selected filter
-  const filteredMessages = messages.filter((msg) => {
-    if (selectedFilter === "New") return true;
-    if (selectedFilter === "Archived") return msg.status === "Archived";
-    return false;
-  });
-
-  // Filter messages based on the search query
-  const filteredBySearch = filteredMessages.filter((msg) =>
-    msg.message.toLowerCase().includes(searchQuery.toLowerCase())
+  const filteredMessages = messages.filter((msg) =>
+    selectedFilter === "New" ? true : msg.status === selectedFilter
   );
 
   return (
@@ -52,15 +42,6 @@ function Messages() {
         {/* Side Nav containing messages */}
         <div className="w-full md:w-1/3 lg:w-1/3 p-4 border-r border-[#252625] overflow-hidden md:block hidden">
           <div className="flex flex-col space-y-6">
-            <Input
-              label=""
-              name="search"
-              placeholder="Search"
-              type="text"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)} // Handle search query change
-            />
-
             <div className="flex flex-wrap gap-2">
               {/* Filter Buttons */}
               {["New", "Archived"].map((filter) => (
@@ -77,7 +58,7 @@ function Messages() {
             </div>
 
             {/* Display Filtered Messages */}
-            {filteredBySearch.map((msg) => (
+            {filteredMessages.map((msg) => (
               <ContactMessage
                 key={msg.id}
                 imgSrc={userProfile2}
@@ -92,7 +73,6 @@ function Messages() {
         <div className="w-full md:w-2/3 lg:w-2/3 flex flex-col h-full">
           <div className="flex items-center justify-between p-4 border-b border-[#252625]">
             <div className="flex items-center space-x-3">
-              {/* back button */}
               <ChevronLeft className="block md:hidden" />
               <Image
                 src={userProfile2}
@@ -112,51 +92,22 @@ function Messages() {
             </div>
           </div>
 
-          <div className="flex-grow p-4 overflow-auto">
-            {/* Add chat messages here */}
-          </div>
-          <div className="flex justify-end p-4">
-            <div className="bg-[#1e1e1e] p-4 rounded-md">
-              <p className="text-[14px] text-[#bbb]">
-                What videos and materials can you recommend for a beginner
-                started web design
-              </p>
-              <div className="flex justify-end items-center space-x-2 mt-2">
-                <small className="text-[#888]">12:43 AM</small>
-                <CheckCheck color="white" size={18} />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex justify-end p-4">
-            <div className="bg-[#1e1e1e] p-4 rounded-md">
-              <p className="text-[14px] text-[#bbb]">
-                You could start with introduction to design by Flora Osatuyi
-              </p>
-              <div className="flex justify-end items-center space-x-2 mt-2">
-                <small className="text-[#888]">12:45 AM</small>
-                <CheckCheck color="white" size={18} />
-              </div>
-            </div>
-          </div>
-
+          <div className="flex-grow p-4 overflow-auto"></div>
           <div className="p-4 flex items-center space-x-4">
             <div className="p-1 pl-4 pr-4 flex items-center space-x-4 border-[#252625] border rounded-[0.5em] w-full">
               <div className="flex space-x-4">
                 <Mic size={24} color="#555" className="cursor-pointer" />
                 <Smile size={24} color="#555" className="cursor-pointer" />
               </div>
-
               <input
                 type="text"
                 placeholder="Type a message..."
                 className="flex-grow p-2 border-l border-r bg-transparent border-[#252625] text-[#555] placeholder:text-[#888] placeholder:italic focus:outline-none focus:border-[#555]"
               />
-
               <Send
                 size={34}
                 color="#555"
-                className="cursor-pointer  bg-[#A8C789] hover:bg-[#3a3a3a] hover:text-white p-2 rounded-full transition-colors duration-200 ease-in-out"
+                className="cursor-pointer bg-[#A8C789] hover:bg-[#3a3a3a] hover:text-white p-2 rounded-full transition-colors duration-200 ease-in-out"
               />
             </div>
           </div>
@@ -186,16 +137,13 @@ const ContactMessage = ({
           className="object-cover w-full h-full"
         />
       </div>
-
       <div className="flex flex-col w-full pr-4 pb-2 border-b border-[#252625]">
         <div className="flex items-center space-x-2">
           <h4 className="text-xs font-semibold text-[#b9b9b9]">{name}</h4>
         </div>
-
         <div className="text-xs text-[#727272] flex items-center space-x-3">
           <CheckCheck color="white" size={18} />
           <p className="truncate text sm w-full">{message}</p>
-
           <div className="flex flex-col items-end ml-4">
             <small className="text-[#bbb]">Friday</small>
             <EllipsisVertical
