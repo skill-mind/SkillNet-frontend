8726cbe0463566e9c2ba59dd7cbb230e7da35876
frankie-powers <franklin.power21@gmail.com>
2025-02-28 07:11:24 +0100

    notification component

--- src/app/dashboard/employer/notifications/notifications.tsx
+++ src/app/dashboard/employer/notifications/notifications.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client";
 
-const Notifications = () => {
+import { useState } from "react";
+import Messages from "./components/Messages";
+import Payments from "./components/Payments";
+import Newrequests from "./components/Newrequests";
+import Announcements from "./components/Announcements";
+
+const tabs = [
+  { id: "messages", label: "Messages" },
+  { id: "payments", label: "Payments" },
+  { id: "announcements", label: "Announcements" },
+  { id: "newrequests", label: "New Requests" },
+] as const;
+
+type TabType = (typeof tabs)[number]["id"];
+
+function Notification() {
+  const [activeTab, setActiveTab] = useState<TabType>("messages");
+
+  const renderComponent = () => {
+    switch (activeTab) {
+      case "messages":
+        return <Messages />;
+
+      case "payments":
+        return <Payments />;
+
+      case "announcements":
+        return <Announcements />;
+
+      case "newrequests":
+        return <Newrequests />;
+
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div>Notifications</div>
-  )
+    <div className="w-full h-full mx-auto p-3 sm:p-6">
+      <header className="w-full flex flex-wrap overflow-x-auto gap-2 sm:gap-4 mb-4">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`py-2 px-3 sm:px-4 text-xs sm:text-sm font-medium rounded-full flex-shrink-0 transition-all ${
+              activeTab === tab.id
+                ? "bg-[#2D2E2D] text-white"
+                : "bg-[#161716] text-gray-400 hover:text-gray-300"
+            }`}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </header>
+
+      <div className="text-white flex flex-col gap-2 py-5 px-4">
+        {renderComponent()}
+      </div>
+    </div>
+  );
 }
 
-export default Notifications
+export default Notification;
