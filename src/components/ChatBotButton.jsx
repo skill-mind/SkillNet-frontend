9ad160643c7fdbb95457958eb9f06204ba17ebe8
Bioba <97415163+Agbeleshe@users.noreply.github.com>
2025-02-28 22:41:41 +0000

    build completed

--- src/components/ChatBotButton.jsx
+++ src/components/ChatBotButton.jsx
@@ -1,6 +1,5 @@
 // components/ChatBotButton.tsx
 import React, { useState } from 'react';
-import { MessageSquare } from 'lucide-react';
 import ChatbotModal from './ChatbotModal';
 import robotic from '../public/img/robotic.svg'
 import Image from 'next/image';
@@ -18,7 +17,7 @@ const ChatBotButton = () => {
         className="flex items-center px-4 py-2 text-gray-300 hover:text-white hover:bg-[#FFFFFF1A] rounded-lg transition-colors w-full"
       >
 
-        <Image className='mr-3' src={robotic} size={20} />
+        <Image className='mr-3' alt='robot icon' src={robotic} size={20} />
         <span>AI Chatbot</span>
       </button>
       <ChatbotModal isOpen={isModalOpen} onClose={closeModal} />
